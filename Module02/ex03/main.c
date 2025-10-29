9fff6f0b8d4976bbd94e3b8af80b35ad323e6dc8
Nemo De Chabalier <nde-chab@f1br4s4.clusters.42paris.fr>
2025-10-29 18:23:01 +0100

    module 02 ex03

--- Module02/ex03/main.c
+++ Module02/ex03/main.c
@@ -21,28 +21,47 @@ void uart_init(){
     UBRR0L = (uint8_t)UBRR_VALUE;
     UBRR0H = (uint8_t)(UBRR_VALUE >> 8);
     //Activer la transmission (TXEN0) dans UCSR0B
-    UCSR0B = (1 << TXEN0) | (1 << RXEN0);
-    //Configurer le mode asynchrone 8N1 dans UCSR0C
+    UCSR0B = (1 << TXEN0) | (1 << RXEN0) | (1 << RXCIE0);
+    //Configurer le mode asynchrone 8N1 dans UCSR0C RXCIE0 = mode interupt sur reception
     UCSR0C = (1 << UCSZ01) | (1 << UCSZ00);
     //activer le double speed 
 }
 
-
+char uart_rx() {
+    while (!(UCSR0A & (1 << RXC0)));
+    char c = UDR0;
+    return c;
+}
 
 void uart_tx(char c){
     //Attendre que UDRE0 (USART Data Register Empty) soit à 1
-            if (c == '\n' || c == '\r') {
-            uart_tx('\r');
-            uart_tx('\n');
-        }
         while (!(UCSR0A & (1 << UDRE0)));
         //Écrire c dans UDR0
         UDR0 = c;
 }
 
+char convert_char(char c) {
+    if (c >= 97 && c <= 122)
+        return (c - 32);
+    else if (c >= 65 && c <=90)
+        return (c + 32);
+    else 
+        return c;
+}
+
+ISR(TIMER1_COMPA_vect) {
+    char c = UDR0;
+    c = convert_char(c);
+    if (c == '\n' || c == '\r') {
+        uart_tx('\r');
+        uart_tx('\n');
+    }
+    else
+        uart_tx(c);
+}
+
 int main(void) {
     uart_init();
-    sei();
     while (1) 
     {
     }
